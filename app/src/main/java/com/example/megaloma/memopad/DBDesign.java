b963cb835061925b93584fcb1ab36b8ae7abb47a
megaloma <megaloma2154@example.com>
2018-10-14 09:56:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/megaloma/memopad/DBDesign.java
@@ -0,0 +1,17 @@
+package com.example.megaloma.memopad;
+
+import android.provider.BaseColumns;
+
+public final class DBDesign {
+
+    //インスタンス化しても何も起きないよう作成
+    private DBDesign(){}
+
+    //テーブル定義の記入
+    public static class MemoTable implements BaseColumns {
+        public static final String _ID = "id";
+        public static final String COLUMN_NAME_MEMO_DATE = "write_date";
+        public static final String COLUMN_NAME_MEMO_TITLE = "memo_title";
+        public static final String COLUMN_NAME_MEMO_CONTENT = "memo_content";
+    }
+}
---
+++ app/src/main/java/com/example/megaloma/memopad/SQLiteHelper.java
@@ -0,0 +1,41 @@
+package com.example.megaloma.memopad;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.example.megaloma.memopad.DBDesign.MemoTable;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+    // データーベースのバージョン
+    private static final int DATABASE_VERSION = 1;
+
+    // データーベース名
+    private static final String DATABASE_NAME = "Memo.db";
+    private static final String TABLE_NAME = "MEMO_DB";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    MemoTable._ID + " INTEGER PRIMARY KEY," +
+                    MemoTable.COLUMN_NAME_MEMO_DATE + "TEXT, " +
+                    MemoTable.COLUMN_NAME_MEMO_TITLE + " TEXT," +
+                    MemoTable.COLUMN_NAME_MEMO_CONTENT + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    public SQLiteHelper(Context context) {
+        super(context,DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
