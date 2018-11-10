db6c152992ac5fc8fa84b9aa909529eec200581d
megaloma <megaloma2154@example.com>
2018-11-11 02:48:58 +0900

    DBHelper(SELECTとINSERT)作成、Activityによる呼び出しを実装 CodeAnalysisからの指摘を修正

--- app/src/main/java/com/example/megaloma/memopad/DBDesign.java
+++ app/src/main/java/com/example/megaloma/memopad/DBDesign.java
@@ -2,17 +2,17 @@ package com.example.megaloma.memopad;
 
 import android.provider.BaseColumns;
 
-public final class DBDesign {
+final class DBDesign {
 
     //インスタンス化しても何も起きないよう作成
     private DBDesign(){}
 
     //テーブル定義の記入
-    public static class MemoTable implements BaseColumns {
-        public static final String _ID = "id";
-        public static final String COLUMN_NAME_MEMO_DATE = "write_date";
-        public static final String COLUMN_NAME_MEMO_TITLE = "memo_title";
-        public static final String COLUMN_NAME_MEMO_CONTENT = "memo_content";
-        public static final String COLUMN_NAME_MEMO_FAV = "fav";
+    static class MemoTable implements BaseColumns {
+        static final String _ID = "id";
+        static final String MEMO_DATE = "write_date";
+        static final String MEMO_TITLE = "title";
+        static final String MEMO_CONTENT = "content";
+        static final String MEMO_FAV = "fav";
     }
 }
--- app/src/main/java/com/example/megaloma/memopad/SQLiteHelper.java
+++ app/src/main/java/com/example/megaloma/memopad/SQLiteHelper.java
@@ -1,6 +1,8 @@
 package com.example.megaloma.memopad;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -13,20 +15,20 @@ public class SQLiteHelper extends SQLiteOpenHelper {
 
     // データーベース名
     private static final String DATABASE_NAME = "Memo.db";
-    private static final String TABLE_NAME = "MEMO_DB";
+    private static final String TABLE_NAME = "MEMO";
 
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + TABLE_NAME + " (" +
-                    MemoTable._ID + " INTEGER PRIMARY KEY," +
-                    MemoTable.MEMO_DATE + "TEXT, " +
+                    MemoTable._ID + " INTEGER PRIMARY KEY, " +
+                    MemoTable.MEMO_DATE + " TEXT, " +
                     MemoTable.MEMO_TITLE + " TEXT, " +
                     MemoTable.MEMO_CONTENT + " TEXT, " +
-                    MemoTable.MEMO_FAV + "TEXT)";
+                    MemoTable.MEMO_FAV + " TEXT)";
 
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + TABLE_NAME;
 
-    public SQLiteHelper(Context context) {
+    SQLiteHelper(Context context) {
         super(context,DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -38,5 +40,36 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    void memoInsert(SQLiteDatabase db, String date, String title, String content){
+        ContentValues values = new ContentValues();
+        values.put("write_date", date); //DB設定後修正
+        values.put("title", title);
+        values.put("content", content);
+        db.insert(TABLE_NAME, null, values);
+    }
+
+    //SELECTを発行する
+    String selectMemo(SQLiteDatabase db){
+        String selectSql = "SELECT * FROM " + TABLE_NAME;
+        try (Cursor cursor = db.rawQuery(selectSql, null)) {
+            return readCursor(cursor);
+        }
+    }
+
+    //検索結果を読み込む
+    private String readCursor(Cursor cursor ){
+        //カーソル開始位置を先頭にする
+        cursor.moveToFirst();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= cursor.getCount(); i++) {
+            //SQL文の結果から、必要な値を取り出す
+            sb.append(cursor.getString(1));
+            sb.append(cursor.getString(2));
+            sb.append(cursor.getString(3));
+            cursor.moveToNext();
+        }
+        return sb.toString();
     }
 }
