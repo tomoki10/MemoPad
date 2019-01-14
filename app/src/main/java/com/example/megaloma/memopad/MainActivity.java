d29c14cb8198f34141b61071f013884b9b18b69a
tomoki10 <tomoki.yakinikuteisyoku@gmail.com>
2019-01-15 01:41:37 +0900

    DBアクセスをSQLHelperからRoomに変更

--- app/src/main/java/com/example/megaloma/memopad/MainActivity.java
+++ app/src/main/java/com/example/megaloma/memopad/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.megaloma.memopad;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -13,22 +14,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.example.megaloma.memopad.db.AppRoomDatabase;
+import com.example.megaloma.memopad.db.MemoDetail;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    // DB を操作するためのインスタンス
-    private SQLiteHelper sqLiteHelper;
-    private SQLiteDatabase sqLiteDatabase;
     // 画面上に表示されるメモを保持するリスト
     ArrayAdapter<String> adapter;
     ListView listView;
+
+    //Room呼び出し
+    AppRoomDatabase appRoomDatabase;
+    //SQL非同期実行用
+    MemoGetTask memoGetTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +43,10 @@ public class MainActivity extends AppCompatActivity
 
         //メモ追加用のボタン
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //詳細画面に移動
-                Intent intent = new Intent(getApplication(), MemoDetailActivity.class);
-                startActivity(intent);
-            }
+        fab.setOnClickListener(view -> {
+            //詳細画面に移動
+            Intent intent = new Intent(getApplication(), MemoDetailActivity.class);
+            startActivity(intent);
         });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -62,42 +63,17 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
-        //DB呼び出し
-        sqLiteHelper     = new SQLiteHelper(this);
-        sqLiteDatabase = sqLiteHelper.getWritableDatabase();
 
-        //データの格納(今後DAO化)
-        final List<String> selectId = sqLiteHelper.selectMemo(sqLiteDatabase,"id");
-        List<String> selectTitle = sqLiteHelper.selectMemo(sqLiteDatabase,"title");
-        List<String> selectDate = sqLiteHelper.selectMemo(sqLiteDatabase,"write_date");
+        //書き換え
+        memoGetTask = new MemoGetTask();
+        memoGetTask.execute((Void) null);
 
-        //取得したDBのカラムをセット
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-
-        for(int i=0;i<selectTitle.size();i++){
-            adapter.add(selectDate.get(i) + "　" + selectTitle.get(i));
-        }
-        listView = findViewById(R.id.listView1);
-        listView.setAdapter(adapter);
-
-        //Itemごとにクリックリスナーを設定
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //開きたいメモの番号(レコードのID)を付与して詳細画面に移動
-                Intent intent = new Intent(getApplication(), MemoDetailActivity.class);
-                intent.putExtra("ID", Integer.valueOf(selectId.get(position)));
-                startActivity(intent);
-            }
-        });
     }
 
     //画面遷移時にオブジェクトを初期化
     @Override
     protected void onPause() {
         super.onPause();
-        sqLiteHelper = null;
-        sqLiteDatabase = null;
         adapter = null;
         listView = null;
     }
@@ -155,5 +131,48 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class MemoGetTask extends AsyncTask<Void, Void, Boolean> {
+
+        List<MemoDetail> list;
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            //別画面から遷移している時のため初期化
+            list = null;
+
+            appRoomDatabase = AppRoomDatabase.getDatabase(getApplicationContext());
+            list = appRoomDatabase.memoDetailDao().loadMemoAll();
+            return true;
+        }
+
+        //画面への書き込みと変数の初期化
+        @Override
+        protected void onPostExecute(Boolean success) {
+            super.onPostExecute(success);
+
+            if(success){
+                //取得したDBのカラムをセット
+                adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1);
+
+                for(int i=0;i<list.size();i++){
+                    adapter.add(list.get(i).write_date + "　" + list.get(i).title);
+                }
+                listView = findViewById(R.id.listView1);
+                listView.setAdapter(adapter);
+
+                //Itemごとにクリックリスナーを設定
+                listView.setOnItemClickListener((parent, view, position, id) -> {
+                    //開きたいメモの番号(レコードのID)を付与して詳細画面に移動
+                    Intent intent = new Intent(getApplication(), MemoDetailActivity.class);
+                    intent.putExtra("ID", Integer.valueOf(list.get(position).id));
+                    startActivity(intent);
+                });
+            }
+            //更新後にタスクを初期化
+            memoGetTask = null;
+        }
     }
 }
--- app/src/main/java/com/example/megaloma/memopad/MemoDetailActivity.java
+++ app/src/main/java/com/example/megaloma/memopad/MemoDetailActivity.java
@@ -1,27 +1,33 @@
 package com.example.megaloma.memopad;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
+
+import com.example.megaloma.memopad.db.AppRoomDatabase;
+import com.example.megaloma.memopad.db.MemoDetail;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
 public class MemoDetailActivity extends AppCompatActivity {
 
-    //DB呼び出し
-    SQLiteHelper sqLiteHelper;
-    SQLiteDatabase sqLiteDatabase;
+    //Room呼び出し
+    AppRoomDatabase appRoomDatabase;
+    //SQL非同期実行用
+    MemoDetailGetTask memoDetailGetTask;
+    MemoInsertTask memoInsertTask;
+    MemoUpdateTask memoUpdateTask;
+    MemoDeleteTask memoDeleteTask;
 
     //保存ボタンの実装
     FloatingActionButton fab;
@@ -43,69 +49,44 @@ public class MemoDetailActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        //DB呼び出し
-        sqLiteHelper     = new SQLiteHelper(this);
-        sqLiteDatabase = sqLiteHelper.getWritableDatabase();
 
-        //メモの全体を格納する
-        List<String> selectRow;
-
-        // メモのtitleを格納する
-        String title = "";
-        String content = "";
-
-        editTitle = findViewById(R.id.memo_title);
-        editContent = findViewById(R.id.memo_content);
-
-        //メイン画面のItemから遷移している場合
-        if(getIntent().hasExtra("ID")){
-            final int id = Objects.requireNonNull(getIntent().getExtras()).getInt("ID");
-            selectRow = sqLiteHelper.selectMemo(sqLiteDatabase,"title, content", "id",String.valueOf(id));
-            title = selectRow.get(0);
-            content = selectRow.get(1);
-        }
-
-        //メモのタイトルと内容を設定
-        editTitle.setText(title);
-        editContent.setText(content);
-
-        //ツールバーにタイトルを設定
-        toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle(title);
-        setSupportActionBar(toolbar);
-
+        //書き換え
+        memoDetailGetTask = new MemoDetailGetTask();
+        memoDetailGetTask.execute((Void) null);
 
         //保存ボタンの実装
         fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //メモのタイトルを取得
-                String title = String.valueOf(editTitle.getText());
+        fab.setOnClickListener(view -> {
+            //メモのタイトルを取得
+            String title = String.valueOf(editTitle.getText());
 
-                //メモの内容を取得
-                String content = String.valueOf(editContent.getText());
+            //メモの内容を取得
+            String content = String.valueOf(editContent.getText());
 
-                //DB呼び出し
-                SQLiteHelper sqLiteHelper     = new SQLiteHelper(getBaseContext());
-                SQLiteDatabase sqLiteDatabase = sqLiteHelper.getWritableDatabase();
+            //挿入・更新用のDAO
+            MemoDetail memoDetail = new MemoDetail();
+            memoDetail.title = title;
+            memoDetail.content = content;
 
-                //メイン画面のItemから遷移している場合
-                if(getIntent().hasExtra("ID")){
-                    final int selectId = Objects.requireNonNull(getIntent().getExtras()).getInt("ID");
-                    sqLiteHelper.selectMemo(sqLiteDatabase,"title", "id", String.valueOf(selectId));
-                    sqLiteHelper.updateMemo(sqLiteDatabase, String.valueOf(selectId), title, content);
-                }
-                //新規ボタンから遷移している場合
-                else{
-                    Calendar calender = Calendar.getInstance();
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
-                    sqLiteHelper.insertMemo(sqLiteDatabase, sdf.format(calender.getTime()), title, content);
-                }
-
-                Intent intent = new Intent(getApplication(), MainActivity.class);
-                startActivity(intent);
+            //メイン画面のItemから遷移している場合
+            if(getIntent().hasExtra("ID")){
+                //既存のメモを更新する
+                memoDetail.id = Objects.requireNonNull(getIntent().getExtras()).getInt("ID");
+                memoUpdateTask = new MemoUpdateTask();
+                memoUpdateTask.execute(memoDetail);
             }
+            //新規ボタンから遷移している場合
+            else{
+                //新規の場合は年月日を設定
+                Calendar calender = Calendar.getInstance();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
+                memoDetail.write_date = sdf.format(calender.getTime());
+                memoInsertTask = new MemoInsertTask();
+                memoInsertTask.execute(memoDetail);
+            }
+
+            Intent intent = new Intent(getApplication(), MainActivity.class);
+            startActivity(intent);
         });
     }
 
@@ -113,12 +94,14 @@ public class MemoDetailActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        sqLiteHelper = null;
-        sqLiteDatabase = null;
         fab = null;
         toolbar = null;
         editTitle = null;
         editContent = null;
+        memoDetailGetTask = null;
+        memoInsertTask = null;
+        memoUpdateTask = null;
+        memoDeleteTask = null;
     }
 
 
@@ -132,7 +115,11 @@ public class MemoDetailActivity extends AppCompatActivity {
             //新規ボタンから遷移している場合、削除せず元の画面に戻る
             if(getIntent().hasExtra("ID")){
                 final int deleteId = Objects.requireNonNull(getIntent().getExtras()).getInt("ID");
-                sqLiteHelper.deleteMemo(sqLiteDatabase, "id", String.valueOf(deleteId));
+//                sqLiteHelper.deleteMemo(sqLiteDatabase, "id", String.valueOf(deleteId));
+                MemoDetail memoDetail = new MemoDetail();
+                memoDetail.id =  deleteId;
+                memoDeleteTask = new MemoDeleteTask();
+                memoDeleteTask.execute(memoDetail);
             }
             startActivity(intent);
             return true;
@@ -146,6 +133,115 @@ public class MemoDetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_memo_detail, menu);
         return true;
+    }
+
+    //メモ削除用
+    @SuppressLint("StaticFieldLeak")
+    private class MemoDeleteTask extends AsyncTask<MemoDetail, Void, Boolean>{
+        @Override
+        protected Boolean doInBackground(MemoDetail... memoDetail) {
+            appRoomDatabase = AppRoomDatabase.getDatabase(getApplicationContext());
+            appRoomDatabase.memoDetailDao().deleteMemo(memoDetail[0]);
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            super.onPostExecute(success);
+            memoDeleteTask = null;
+        }
+    }
+
+    //メモ更新用
+    @SuppressLint("StaticFieldLeak")
+    private class MemoUpdateTask extends AsyncTask<MemoDetail, Void, Boolean>{
+        MemoDetail mMemoDetail;
+
+        @Override
+        protected Boolean doInBackground(MemoDetail... memoDetail) {
+            appRoomDatabase = AppRoomDatabase.getDatabase(getApplicationContext());
+            //DAOごと更新するため、一度呼び出す
+            mMemoDetail = appRoomDatabase.memoDetailDao().loadMemo(memoDetail[0].id);
+            mMemoDetail.title = String.valueOf(editTitle.getText());
+            mMemoDetail.content = String.valueOf(editContent.getText());
+            appRoomDatabase.memoDetailDao().updateMemo(mMemoDetail);
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            super.onPostExecute(success);
+            memoUpdateTask = null;
+        }
+
+    }
+
+    //メモ挿入用
+    @SuppressLint("StaticFieldLeak")
+    private class MemoInsertTask extends AsyncTask<MemoDetail, Void, Boolean>{
+        @Override
+        protected Boolean doInBackground(MemoDetail... memoDetail) {
+            appRoomDatabase = AppRoomDatabase.getDatabase(getApplicationContext());
+            appRoomDatabase.memoDetailDao().InsertMemo(memoDetail[0]);
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            super.onPostExecute(success);
+            memoInsertTask = null;
+        }
+
+    }
+
+    //メモ詳細の取得
+    @SuppressLint("StaticFieldLeak")
+    private class MemoDetailGetTask extends AsyncTask<Void, Void, Boolean>{
+
+        //メモの全体を格納する
+        MemoDetail memoDetail;
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+
+            //初期化
+            memoDetail = null;
+            if(getIntent().hasExtra("ID")){
+                int id = Objects.requireNonNull(getIntent().getExtras()).getInt("ID");
+                appRoomDatabase = AppRoomDatabase.getDatabase(getApplicationContext());
+                memoDetail = appRoomDatabase.memoDetailDao().loadMemo(id);
+            }
+            return true;
+        }
+
+        //画面への書き込みと変数の初期化
+        @Override
+        protected void onPostExecute(Boolean success) {
+            super.onPostExecute(success);
+
+            if(success){
+
+                editTitle = findViewById(R.id.memo_title);
+                editContent = findViewById(R.id.memo_content);
+
+                if(getIntent().hasExtra("ID")){
+
+
+                    //メモのタイトルと内容を設定
+                    editTitle.setText(memoDetail.title);
+                    editContent.setText(memoDetail.content);
+
+                    //ツールバーにタイトルを設定
+                    toolbar = findViewById(R.id.toolbar);
+                    toolbar.setTitle(memoDetail.title);
+                    setSupportActionBar(toolbar);
+                }
+
+            }
+            //更新後にタスクを初期化
+            memoDetailGetTask = null;
+        }
+
     }
 
 }
